a501fc301b8820d11bbd8f65701eb195f2a6b864
Tigran <tigran2589@gmail.com>
2022-06-29 22:09:33 +0300

    set up search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,21 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { AlertContext } from "../context/alert/alertContext"
 
 export const Search = () => {
 
+  const [value, setValue] = useState('')
+
   const { show } = useContext(AlertContext)
 
   const onSubmit = event => {
-    if (event.key === 'Enter') {
-      show('This is alert')
+    if (event.key !== 'Enter') {
+      return
+    }
+
+    if (value.trim()) {
+      console.log('Make request with: ', value)
+    } else {
+      show("Enter user's info")
     }
   }
 
@@ -18,6 +26,8 @@ export const Search = () => {
         type="text"
         className="form-control"
         placeholder="Enter Your Nickname"
+        value={value}
+        onChange={event => setValue(event.target.value)}
         onKeyPress={onSubmit}
       />
     </div>
